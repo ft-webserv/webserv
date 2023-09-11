6c5cf9ca18aef19c633ed54f833960e4b12c197b
Hyeonjung Kim <hyeonjun@cx2r6s2.42seoul.kr>
2023-09-11 19:22:27 +0900

    parseRequest() test

--- includes/Server.hpp
+++ includes/Server.hpp
@@ -11,6 +11,19 @@
 #include <map>
 #define BACKLOG SOMAXCONN
 
+typedef struct s_request
+{
+	std::string _method;
+	std::string _location;
+	std::string _httpVersion;
+	std::string _host;
+	std::string _accept;
+	std::string _connection;
+	std::string _contentType;
+	unsigned int _contentLength;
+	std::string _body;
+} t_request;
+
 class Server
 {
 public:
@@ -22,9 +35,11 @@ public:
 	void setHostServer(ServerInfo *serverBlock);
 	std::map<std::string, ServerInfo *> getHostServer();
 	void parseRequest(std::string request);
+	void testPrintRequest(void);
 
 private:
 	std::map<std::string, ServerInfo *> _hostServer;
+	t_request _requestInfo;
 };
 
 #endif
--- sources/Server/Server.cpp
+++ sources/Server/Server.cpp
@@ -30,10 +30,64 @@ void Server::setHostServer(ServerInfo *serverBlock)
 	}
 }
 
+void Server::testPrintRequest(void)
+{
+	std::cout << "Method: " << _requestInfo._method << std::endl;
+	std::cout << "Location: " << _requestInfo._location << std::endl;
+	std::cout << "HTTPVersion: " << _requestInfo._httpVersion << std::endl;
+	std::cout << "HOST: " << _requestInfo._host << std::endl;
+	std::cout << "Accept: " << _requestInfo._accept << std::endl;
+	std::cout << "Connection: " << _requestInfo._connection << std::endl;
+	std::cout << "ContentType: " << _requestInfo._contentType << std::endl;
+	std::cout << "ContentLength: " << _requestInfo._contentLength << std::endl;
+	std::cout << "Body: " << _requestInfo._body << std::endl;
+}
+
 void Server::parseRequest(std::string request)
 {
-	std::cout << _hostServer.begin()->first << std::endl;
-	std::cout << request << std::endl;
+	std::string::size_type pos;
+	std::string word;
+
+	memset(&_requestInfo, 0, sizeof(_requestInfo));
+	for (pos = 0; pos < request.length();)
+	{
+		std::string::size_type pre = pos;
+
+		pos = request.find("\r\n", pos);
+		std::istringstream line(request.substr(pre, pos));
+		line >> word;
+		if (word == "GET" || word == "POST" || word == "DELETE")
+		{
+			_requestInfo._method = word;
+			line >> _requestInfo._location >> _requestInfo._httpVersion;
+		}
+		else if (word == "Host:")
+		{
+			line >> _requestInfo._host;
+		}
+		else if (word == "Accept:")
+		{
+			line >> _requestInfo._accept;
+		}
+		else if (word == "Connection:")
+		{
+			line >> _requestInfo._connection;
+		}
+		else if (word == "Content-Type:")
+		{
+			line >> _requestInfo._contentType;
+		}
+		else if (word == "Content-Length:")
+		{
+			line >> _requestInfo._contentLength;
+		}
+		else if (word == "")
+		{
+			_requestInfo._body = request.substr(pos, request.length());
+		}
+		pos += 2;
+		testPrintRequest();
+	}
 }
 
 std::map<std::string, ServerInfo *> Server::getHostServer()
