e51bae4bf760f5907b40758f52e9e1d6c0d4b224
darjeeling4g <darjeeling4g@gmail.com>
2023-09-21 16:03:01 +0900

    [Webserv] Add: Error class

--- includes/Error.hpp
+++ includes/Error.hpp
@@ -1,5 +1,11 @@
 #pragma once
 
+#include <string>
+#include <fstream>
+#include <sys/stat.h>
+
+#include "Status.hpp"
+
 class Error
 {
 public:
@@ -7,19 +13,26 @@ public:
 	~Error();
 
 protected:
-	void _400badRequest();
-	void _401unauthorized();
-	void _403forbidden();
-	void _404notFound();
-	void _405methodNotAllowed();
-	void _406notAcceptable();
-	void _408requestTimeout();
-	void _409conflict();
-	void _413requestEntityTooLarge();
-	void _414uriTooLong();
-	void _500internalServerError();
-	void _501badGateWay();
-	void _503serviceUnavailable();
-	void _504gatewayTimeout();
-	void _505httpVersionNotSupported();
+	void _sendErrorPage(uintptr_t socket, std::string errorPagePath, eStatus statusCode);
+	// void _400badRequest(uintptr_t socket, std::string errorPagePath);
+	// void _401unauthorized(uintptr_t socket, std::string errorPagePath);
+	// void _403forbidden(uintptr_t socket, std::string errorPagePath);
+	// void _404notFound(uintptr_t socket, std::string errorPagePath);
+	// void _405methodNotAllowed(uintptr_t socket, std::string errorPagePath);
+	// void _406notAcceptable(uintptr_t socket, std::string errorPagePath);
+	// void _408requestTimeout(uintptr_t socket, std::string errorPagePath);
+	// void _409conflict(uintptr_t socket, std::string errorPagePath);
+	// void _413requestEntityTooLarge(uintptr_t socket, std::string errorPagePath);
+	// void _414uriTooLong(uintptr_t socket, std::string errorPagePath);
+	// void _500internalServerError(uintptr_t socket, std::string errorPagePath);
+	// void _501notImplemented(uintptr_t socket, std::string errorPagePath);
+	// void _502badGateWay(uintptr_t socket, std::string errorPagePath);
+	// void _503serviceUnavailable(uintptr_t socket, std::string errorPagePath);
+	// void _504gatewayTimeout(uintptr_t socket, std::string errorPagePath);
+	// void _505httpVersionNotSupported(uintptr_t socket, std::string errorPagePath);
+
+private:
+	std::string _getDefaultPath(eStatus statusCode);
+	std::string _defaultPath[2][13];
+	std::string _response;
 };
--- sources/Error/Error.cpp
+++ sources/Error/Error.cpp
@@ -2,68 +2,109 @@
 
 Error::Error()
 {
+	memset(_defaultPath, 0, sizeof(std::string) * 2 * 13);
+	_defaultPath[0][0] = "";
+	_defaultPath[0][1] = "";
+	_defaultPath[0][3] = "";
+	_defaultPath[0][4] = "";
+	_defaultPath[0][5] = "";
+	_defaultPath[0][6] = "";
+	_defaultPath[0][8] = "";
+	_defaultPath[0][13] = "";
+	_defaultPath[1][0] = "";
+	_defaultPath[1][1] = "";
+	_defaultPath[1][5] = "";
 }
 
 Error::~Error()
 {
 }
 
-void Error::_400badRequest()
+std::string _getDefaultPath(eStatus statusCode)
 {
+	int type = statusCode / 100;
+	int detail = statusCode % 100;
+
+	if (type == 4 || type == 5)
+	{
+		return (_defaultPath[type - 4][detail]);
+	}
+	return ("");
 }
 
-void Error::_401unauthorized()
+void _sendErrorPage(uintptr_t socket, std::string errorPagePath, eStatus statusCode);
 {
+	std::ifstream errorPage;
+	struct stat buf;
+
+	if (errorPagePath.empty)
+	{
+		errorPagePath = _getDefaultPath(statusCode);
+	}
+	stat(errorPagePath.c_str(), &buf);
+	errorPage.open(errorPagePath);
 }
 
-void Error::_403forbidden()
-{
-}
+// void Error::_400badRequest(uintptr_t socket, std::string errorPagePath)
+// {
 
-void Error::_404notFound()
-{
-}
+// }
 
-void Error::_405methodNotAllowed()
-{
-}
+// void Error::_401unauthorized(uintptr_t socket, std::string errorPagePath)
+// {
+// }
 
-void Error::_406notAcceptable()
-{
-}
+// void Error::_403forbidden(uintptr_t socket, std::string errorPagePath)
+// {
+// }
 
-void Error::_408requestTimeout()
-{
-}
+// void Error::_404notFound(uintptr_t socket, std::string errorPagePath)
+// {
+// }
 
-void Error::_409conflict()
-{
-}
+// void Error::_405methodNotAllowed(uintptr_t socket, std::string errorPagePath)
+// {
+// }
 
-void Error::_413requestEntityTooLarge()
-{
-}
+// void Error::_406notAcceptable(uintptr_t socket, std::string errorPagePath)
+// {
+// }
 
-void Error::_414uriTooLong()
-{
-}
+// void Error::_408requestTimeout(uintptr_t socket, std::string errorPagePath)
+// {
+// }
 
-void Error::_500internalServerError()
-{
-}
+// void Error::_409conflict(uintptr_t socket, std::string errorPagePath)
+// {
+// }
 
-void Error::_501badGateWay()
-{
-}
+// void Error::_413requestEntityTooLarge(uintptr_t socket, std::string errorPagePath)
+// {
+// }
 
-void Error::_503serviceUnavailable()
-{
-}
+// void Error::_414uriTooLong(uintptr_t socket, std::string errorPagePath)
+// {
+// }
 
-void Error::_504gatewayTimeout()
-{
-}
+// void Error::_500internalServerError(uintptr_t socket, std::string errorPagePath)
+// {
+// }
 
-void Error::_505httpVersionNotSupported()
-{
-}
+// void Error::_501notImplemented(uintptr_t socket, std::string errorPagePath)
+// {
+// }
+// void Error::_501badGateWay(uintptr_t socket, std::string errorPagePath)
+// {
+// }
+
+// void Error::_503serviceUnavailable(uintptr_t socket, std::string errorPagePath)
+// {
+// }
+
+// void Error::_504gatewayTimeout()
+// {
+// }
+
+// void Error::_505httpVersionNotSupported()
+// {
+// }
