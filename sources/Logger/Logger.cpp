01cbd9b739dec383a2d3b6c5cf094f59268a9f59
Hyeonjung Kim <hyeonjun@cx2r3s3.42seoul.kr>
2023-10-23 17:14:34 +0900

    [Webserv] Refactor: modify logger

--- sources/Logger/Logger.cpp
+++ sources/Logger/Logger.cpp
@@ -21,37 +21,36 @@ void Logger::openedServerInfo(int servSock, int port)
 
 void Logger::clientConnect(uintptr_t clntSock)
 {
-	std::cout << BLUE << "[" << printDate() << "]"
-			  << "\tClient (" << clntSock << ") Connected" << CLEAR << std::endl;
+	printDate();
+	std::cout << BLUE << "\tClient (" << clntSock << ") Connected" << CLEAR << std::endl;
 }
 
 void Logger::clientDisconnect(uintptr_t clntSock)
 {
-	std::cout << RED
-			  << "[" << printDate() << "]"
-			  << "\tClient (" << clntSock << ") Disconnected" << CLEAR << std::endl;
+	printDate();
+	std::cout << RED << "\tClient (" << clntSock << ") Disconnected" << CLEAR << std::endl;
 }
 
 void Logger::serverRecvFromClient(uintptr_t clntSock)
 {
-	std::cout << GREEN << "[" << printDate() << "]"
-			  << "\tServer Has Recived Data From Client (" << clntSock << ")" << CLEAR << std::endl;
+	printDate();
+	std::cout << GREEN << "\tServer Has Recived Data From Client (" << clntSock << ")" << CLEAR << std::endl;
 }
 
 void Logger::serverWriteToCgi(pid_t cgiFd)
 {
-	std::cout << MAGENTA << "[" << printDate() << "]"
-			  << "\tServer Has Written Data to CGI (" << cgiFd << ")" << CLEAR << std::endl;
+	printDate();
+	std::cout << MAGENTA << "\tServer Has Written Data to CGI (" << cgiFd << ")" << CLEAR << std::endl;
 }
 
 void Logger::serverReadFromCgi(pid_t cgiFd)
 {
-	std::cout << CYAN << "[" << printDate() << "]"
-			  << "\tServer Has Read Data From CGI (" << cgiFd << ")" << CLEAR << std::endl;
+	printDate();
+	std::cout << CYAN << "\tServer Has Read Data From CGI (" << cgiFd << ")" << CLEAR << std::endl;
 }
 
 void Logger::serverSendToClient(uintptr_t clntSock)
 {
-	std::cout << YELLOW << "[" << printDate() << "]"
-			  << "\tServer Has Sent Data to Client (" << clntSock << ")" << CLEAR << std::endl;
+	printDate();
+	std::cout << YELLOW << "\tServer Has Sent Data to Client (" << clntSock << ")" << CLEAR << std::endl;
 }
